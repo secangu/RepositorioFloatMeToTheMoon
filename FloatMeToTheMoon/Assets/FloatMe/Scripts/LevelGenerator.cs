5129f0f297d323e0b6acefb3ec07a041ce0df67e
secangu <carvajalsergio2018@outlook.es>
2024-01-27 17:44:02 -0500

    Variantes de nivel Casi Terminadas

--- FloatMeToTheMoon/Assets/FloatMe/Scripts/LevelGenerator.cs
+++ FloatMeToTheMoon/Assets/FloatMe/Scripts/LevelGenerator.cs
@@ -1,66 +1,104 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace FloatMeToTheMoon
 {
     public class LevelGenerator : MonoBehaviour
     {
-        [SerializeField] private GameObject[] levelParts;
-        [SerializeField] private Transform endPoint;
-        [SerializeField] private float minDistance;
-        [SerializeField] private GameObject finalPart; // Nueva variable para la parte final
-        [SerializeField] private bool infinity; // Nueva variable para la parte final
+        public enum LevelPartType
+        {
+            Initial,
+            Part2,
+            Part3,
+            Part4,
+            Part5,
+            Part6,
+            Part7,
+            Part8,
+            Part9,
+            Part10,
+            Final
+        }
 
-        private int currentIndex;
+        [System.Serializable]
+        public struct LevelPartPrefab
+        {
+            public LevelPartType partType;
+            public GameObject[] prefabs;
+        }
+
+        [SerializeField] private List<LevelPartType> levelOrder;
+        [SerializeField] private List<LevelPartPrefab> levelParts;
+        [SerializeField] private Transform initialEndPoint;
+        [SerializeField] private float minDistance;
+        [SerializeField] private bool infinity;
+
         private Transform player;
+        private Transform endPoint;
 
         private void Awake()
         {
-            player = GameObject.FindGameObjectWithTag("Player").transform;
+            player = GameObject.FindGameObjectWithTag("Player")?.transform;
+            if (player == null)
+            {
+                Debug.LogError("Player null");
+            }
         }
 
         private void Start()
         {
-            currentIndex = 0;
+            endPoint = initialEndPoint;
 
-
-            for (int i = 0; i < levelParts.Length; i++)
+            foreach (LevelPartType partType in levelOrder)
             {
-                GenerateLevelParts();
+                if (infinity && partType == LevelPartType.Final)
+                {
+                    // Evitar la generación de la parte final si infinity está activo
+                    break;
+                }
+                GenerateLevelPart(partType);
+                
             }
-                GenerateFinalPart(); // Después de generar las partes iniciales, agrega la parte final
 
+            //if (!infinity) GenerateLevelPart(LevelPartType.Final);
         }
 
         private void Update()
         {
-            //Para hacer el nivel infinito
             if (infinity && Vector2.Distance(player.position, endPoint.position) < minDistance)
             {
-                GenerateFinalPart();
+                // Genera aleatoriamente entre la parte 9 y 10
+                LevelPartType randomPartType = Random.Range(0, 2) == 0 ? LevelPartType.Part9 : LevelPartType.Part10;
+                GenerateLevelPart(randomPartType);
+            }
+
+        }
+
+        private void GenerateLevelPart(LevelPartType partType)
+        {
+            LevelPartPrefab partPrefab = levelParts.Find(x => x.partType == partType);
+
+            if (partPrefab.prefabs != null && partPrefab.prefabs.Length > 0)
+            {
+                int randomIndex = Random.Range(0, partPrefab.prefabs.Length);
+                GameObject levelPart = Instantiate(partPrefab.prefabs[randomIndex], endPoint.position, Quaternion.identity);
+                endPoint = FindEndPoint(levelPart);
+            }
+            else
+            {
+                Debug.LogError("Prefab en array no esta asignado para LevelPartType: " + partType);
             }
         }
 
-        private void GenerateLevelParts()
-        {
-            GameObject level = Instantiate(levelParts[currentIndex], endPoint.position, Quaternion.identity);
-            endPoint = FindEndPoint(level);
-
-            currentIndex++; // Avanzamos al siguiente índice en la lista
-        }
-        private void GenerateFinalPart()
-        {
-            GameObject finalLevel = Instantiate(finalPart, endPoint.position, Quaternion.identity);
-            endPoint = FindEndPoint(finalLevel);
-        }
         private Transform FindEndPoint(GameObject levelPart)
         {
             Transform point = null;
 
-            foreach (Transform ubication in levelPart.transform)
+            foreach (Transform location in levelPart.transform)
             {
-                if (ubication.CompareTag("EndPoint"))
+                if (location.CompareTag("EndPoint"))
                 {
-                    point = ubication;
+                    point = location;
                     break;
                 }
             }
